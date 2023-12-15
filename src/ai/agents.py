69c844ca09f57e974e0a0e15572ee1914c047109
Dirk Brand <dirk@primer.io>
2023-12-15 20:16:36 +0200

    Ok now they can counter

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -1,13 +1,11 @@
-from autogen import UserProxyAgent, AssistantAgent
-from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
+from autogen import AssistantAgent, UserProxyAgent
 
 from src.handler.game_handler import ResistanceCoupGameHandler
 from src.models.action import ActionType
 from src.models.card import Card
 
-
-
 # SEED = 42
+
 
 def create_user_proxy(config_list: list) -> UserProxyAgent:
     llm_config = {
@@ -21,7 +19,8 @@ def create_user_proxy(config_list: list) -> UserProxyAgent:
         llm_config=llm_config,
         is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
         system_message="""
-            You are facilitating a game of The Resistance: Coup between five players. Respond with TERMINATE once the game has a winner.
+            You are facilitating a game of The Resistance: Coup between five players. 
+            Respond with TERMINATE once the game has a winner.
             At the start of the game, you will inform the starting player that it is their turn.
             In between each player's turn you have to retrieve the game state and provide it to the players.
             """,
@@ -33,7 +32,9 @@ def create_user_proxy(config_list: list) -> UserProxyAgent:
     return user_proxy
 
 
-def create_game_master_agent(handler: ResistanceCoupGameHandler, config_list: list) -> AssistantAgent:
+def create_game_master_agent(
+    handler: ResistanceCoupGameHandler, config_list: list
+) -> AssistantAgent:
     llm_config = {
         "config_list": config_list,
         # "seed": SEED,
@@ -44,21 +45,24 @@ def create_game_master_agent(handler: ResistanceCoupGameHandler, config_list: li
                 "description": "Get the current state of the game",
                 "parameters": {
                     "type": "object",
-                    "properties": {
-                    },
-                }
+                    "properties": {},
+                },
             },
-        ]
+        ],
     }
 
     instructions = f"""
-    You are the game master in a game of The Resistance: Coup between {handler.number_of_players} players.
+    You are the game master in a game of The Resistance: Coup between {len(handler.players)} players.
     
-    At the start of the game, you will inform the starting player that it is their turn and announce to everyone the starting game state.
+    At the start of the game, you will inform the starting player that it is their turn and announce to 
+    everyone the starting game state.
     
     In between each player's turn you have to retrieve the game state and provide it to the current player.
     
     Make sure the correct player is taking their next turn based on the game state.
+    
+    Players can counter another player's action if action_can_be_countered is "True" after they 
+    tried to perform an action.
     
     Once there is only one active player left in the game, you can declare the game is over and we have a winner.
     """
@@ -71,17 +75,23 @@ def create_game_master_agent(handler: ResistanceCoupGameHandler, config_list: li
             "get_game_state": handler.get_game_state,
         },
         # max_consecutive_auto_reply=100,
-        description="The game master in a game of The Resistance Coup"
+        description="The game master in a game of The Resistance Coup",
     )
     return game_master
 
 
-def create_player_agent(name: str, other_player_names: list[str], cards: list[Card], strategy: str,
-                        handler: ResistanceCoupGameHandler, config_list: list) -> AssistantAgent:
+def create_player_agent(
+    name: str,
+    other_player_names: list[str],
+    cards: list[Card],
+    strategy: str,
+    handler: ResistanceCoupGameHandler,
+    config_list: list,
+) -> AssistantAgent:
     llm_config = {
         "config_list": config_list,
         # "seed": SEED,
-        "temperature": 0.3,
+        "temperature": 0.4,
         "functions": [
             {
                 "name": "perform_action",
@@ -92,26 +102,77 @@ def create_player_agent(name: str, other_player_names: list[str], cards: list[Ca
                         "player_name": {
                             "type": "string",
                             "description": "Send your own name.",
-                            "enum": [name]
+                            "enum": [name],
                         },
                         "action_name": {
                             "type": "string",
                             "description": "The name of the action to perform.",
-                            "enum": [ActionType.income, ActionType.foreign_aid, ActionType.tax, ActionType.coup,
-                                     ActionType.steal, ActionType.assassinate, ActionType.exchange]
+                            "enum": [
+                                ActionType.income,
+                                ActionType.foreign_aid,
+                                ActionType.tax,
+                                ActionType.coup,
+                                ActionType.steal,
+                                ActionType.assassinate,
+                                ActionType.exchange,
+                            ],
                         },
                         "target_player_name": {
                             "type": "string",
                             "description": "The player name to target.",
                         },
                     },
-                    "required": [
-                        "player_name",
-                        "action_name"
-                    ]
-                }
-            }
-        ]
+                    "required": ["player_name", "action_name"],
+                },
+            },
+            {
+                "name": "counter_action",
+                "description": "Counter the previous action that was performed",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "countering_player_name": {
+                            "type": "string",
+                            "description": "Send your own name.",
+                            "enum": [name],
+                        },
+                    },
+                    "required": ["countering_player_name"],
+                },
+            },
+            {
+                "name": "execute_action",
+                "description": "Execute the action that was performed and complete the turn.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "player_name": {
+                            "type": "string",
+                            "description": "Send your own name.",
+                            "enum": [name],
+                        },
+                        "action_name": {
+                            "type": "string",
+                            "description": "The name of the action to perform.",
+                            "enum": [
+                                ActionType.income,
+                                ActionType.foreign_aid,
+                                ActionType.tax,
+                                ActionType.coup,
+                                ActionType.steal,
+                                ActionType.assassinate,
+                                ActionType.exchange,
+                            ],
+                        },
+                        "target_player_name": {
+                            "type": "string",
+                            "description": "The player name to target.",
+                        },
+                    },
+                    "required": ["player_name", "action_name"],
+                },
+            },
+        ],
     }
 
     instructions = f"""Your name is {name} and you are a player in the game The Resistance: Coup. 
@@ -119,17 +180,24 @@ def create_player_agent(name: str, other_player_names: list[str], cards: list[Ca
         
         You start with a {str(cards[0])} card and a {str(cards[1])} card, as well as 2 coins.
         
-        On your turn you have to pick a valid action based on your current available cards and coins. Also provide your own name to the function. 
+        On your turn you have to pick a valid action based on your current available cards and coins. 
+        Also provide your own name to the function. 
         
         Never announce what cards you have, they are secret.
         
-        If your action was invalid, you have to pick another action. However feel free to bluff and perform an action even if you don't have the card.
+        If your action was invalid, you have to pick another action. However feel free to bluff and perform an 
+        action even if you don't have the card.
         
-        The possible actions are {[ActionType.income, ActionType.foreign_aid, ActionType.tax, ActionType.coup, ActionType.steal, ActionType.assassinate, ActionType.exchange]}
+        You can counter another player's action if action_can_be_countered is "True", 
+        after they tried to perform their action.
         
+        If no one counters your action, you have the call "execute_action" to complete the turn. 
+        If after perform_action you find that turn_complete is "True", you don't have to execute your action.
+                
         You also chit-chat with your opponent when you communicate an action to light up the mood.
 
-        You should ensure both you and your opponents are making valid actions. Also that everyone is only taking actions when it is their turn.
+        You should ensure both you and your opponents are making valid actions. 
+        Also that everyone is only taking actions when it is their turn.
         
         Your strategy should be to play {strategy}.
         
@@ -145,9 +213,11 @@ def create_player_agent(name: str, other_player_names: list[str], cards: list[Ca
         llm_config=llm_config,
         function_map={
             "perform_action": handler.perform_action,
+            "counter_action": handler.counter_action,
+            "execute_action": handler.execute_action,
         },
         max_consecutive_auto_reply=100,
-        description=f"The player named {name} the game of The Resistance Coup"
+        description=f"The player named {name} the game of The Resistance Coup",
     )
 
     return player
